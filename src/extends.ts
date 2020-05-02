2c20fe978cd6d3425c7077de7216b62c4832c618
giotramu <14157578+giotramu@users.noreply.github.com>
2020-05-02 17:35:47 +0200

    chore(package): rewrite test script

--- src/extends.ts
+++ src/extends.ts
@@ -2,16 +2,16 @@ import {mergeObjects} from './_helpers';
 import {Options, PostcssConfig, Plugins} from './_types';
 import {getPostcssConfig} from './config';
 import {debugConfig} from './debug';
-import {checkOptions} from './options';
-import {pluginEncoder} from './plugins';
+import {optionsParser} from './options';
+import {pluginsParser} from './plugins';
 
 export = extendsConfig;
 
 function extendsConfig(plugins: Plugins, options?: Options): PostcssConfig {
-  const {debug, sourceMap, browsers} = checkOptions(options);
+  const {debug, sourceMap, browsers} = optionsParser(options);
   const source = getPostcssConfig({browsers, sourceMap});
 
-  const parsedPlugins = pluginEncoder(plugins);
+  const parsedPlugins = pluginsParser(plugins);
   const extendedPlugins = mergeObjects(source.plugins, parsedPlugins);
   const config = {...source, plugins: extendedPlugins};
 
