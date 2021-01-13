6d2b2e416a250f7fde64807aa4626e52561afa8d
giotramu <14157578+giotramu@users.noreply.github.com>
2021-01-13 16:31:33 +0100

    Update dependencies for support postcss v8

--- src/common/config.ts
+++ src/common/config.ts
@@ -7,15 +7,15 @@ export function getPostcssConfig({
 }: ConfigOptions): PostcssConfig {
   return {
     map: sourceMap,
-    syntax: 'postcss-scss',
+    // syntax: 'postcss-scss', // --- FIXME: Uncomment after this PR was closed: https://github.com/jonathantneal/postcss-sass/issues/24
     plugins: {
       // --- The plugins execution order is top-down
-      '@csstools/postcss-sass': settings.postsass(),
-      'postcss-selector-not': true,
+      // '@csstools/postcss-sass': settings.postsass(), // --- FIXME: Uncomment after this PR was closed: https://github.com/jonathantneal/postcss-sass/issues/24
+      // 'postcss-selector-not': true, // --- FIXME: Uncomment after this PR was closed: https://github.com/postcss/postcss-selector-not/pull/16
       'postcss-custom-media': true,
       'postcss-inline-svg': settings.inlinesvg(),
       autoprefixer: settings.autoprefixer(browsers),
-      cssnano: settings.cssnano(),
+      // cssnano: settings.cssnano(), // --- FIXME: Uncomment after the cssnano package was bumped
       'postcss-reporter': settings.reporter()
     }
   };
